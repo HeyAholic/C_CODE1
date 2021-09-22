7d145ab8b12a84f90c21d728dd151efcdb3af011
HeyAholic <1976822450@qq.com>
2021-09-22 17:34:38 +0800

    添加项目文件。

---
+++ 递归/text.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<string.h>
+#include<stdio.h>
+// ฒยวเอÜ
+// 
+int my_tiao()
+{
+	
+}
+
+
+
+
+
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+	int ret=my_tiao();
+	printf("%d\n", ret);
+	return 0;
+}
